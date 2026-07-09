71d17f05655bf1ac4c277c9459731ae2cb3dae2b
Juan Menezes <jccm082712@gmail.com>
2026-07-09 21:41:50 +0000

    Altera a frase da página (Teste Vercel)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Flamengo</h1>
+  return (
+    <main
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        fontFamily: "Arial, sans-serif",
+        backgroundColor: "#f5f5f5",
+      }}
+    >
+      <h1>Teste Site hospedado na Vercel</h1>
+    </main>
+  );
 }
 
 export default Home;
