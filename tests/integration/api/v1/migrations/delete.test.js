df38fbc7112844b04034b4ada58a9d74718b906a
Juan Menezes <jccm082712@gmail.com>
2026-08-11 16:58:51 +0000

    Adiciona testes PUT e DELETE ao endpoint `/migrations`

---
+++ tests/integration/api/v1/migrations/delete.test.js
@@ -0,0 +1,22 @@
+import database from "infra/database.js";
+
+test("POST to api/v1/migrations should return 405", async () => {
+  const response1 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+  expect(response1.status).toBe(405);
+
+  const response2 = await fetch("http://localhost:3000/api/v1/migrations", {
+    method: "DELETE",
+  });
+  expect(response1.status).toBe(405);
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*) FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName],
+  });
+  const openedConnections = parseInt(openedConnectionsResult.rows[0].count);
+
+  expect(openedConnections).toBe(1);
+});
